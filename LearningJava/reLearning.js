b01905d955db304133c2d9c1f642c2b6379a7414
EnriqueLopez63 <111899612+EnriqueLopez63@users.noreply.github.com>
2022-11-16 14:54:58 -0600

    Add files via upload

---
+++ LearningJava/reLearning.js
@@ -0,0 +1,16 @@
+import datetime
+
+alert('yessuhhhhh');
+console.log("console this");
+
+let age = (17)
+let year = (2022)
+console.log(age , year)
+
+age = 99
+console.log(age)
+const points = 100;
+
+for (let i = 0; i < 5; i++){
+    console.log("for loop")
+}
