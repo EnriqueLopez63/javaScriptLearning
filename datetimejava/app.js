4e6b9e35072916f81f4a2e7a5517dbb47e5ce5a5
EnriqueLopez63 <111899612+EnriqueLopez63@users.noreply.github.com>
2022-11-17 19:58:18 -0800

    Add files via upload

---
+++ datetimejava/app.js
@@ -0,0 +1,68 @@
+
+let a = new Date()
+let ams = a.getTime()
+
+let cd = new Date("November 21 2022")
+let cdms = cd.getTime()
+
+let s = cdms - ams 
+let msd = 60*60*24*1000
+
+console.log(Math.floor(s/msd))
+
+
+
+
+let today = new Date()
+let yuh = today.getTime()
+
+let variable = new Date("December 5 2022")
+let idk = variable.getTime()
+
+let math = idk - yuh 
+let timemath = 60*60*24*1000
+
+console.log(Math.floor(math/timemath))
+
+
+let ad = new Date();
+console.log(ad)
+
+let bd = prompt("enter ur birthday");
+console.log (bd)
+
+var adbd = ad - bd;
+console.log(adbd);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////
+
+
+// let a = new Date(askDate)
+// let sec = a.getTime()
+// console.log(sec)
+
+// let currentdate = new Date();
+// console.log(currentdate);
+
+// let milliseconds = new Date ("june 17 2005")
+// console.log(dateString);
+
+// let dateString = new Date ("sep 11 2001 5:14:00")
+// console.log(dateString);
+
+// let componentDate = new Date (2001, 08, 11, 5, 14, 0, 0)
+// console.log(dateString);
